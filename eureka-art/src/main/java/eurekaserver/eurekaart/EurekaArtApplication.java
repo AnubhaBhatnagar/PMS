70acf7d77cbee8573c985dd3c02ad7e2d7597016
849338 <849338@LTIN177415.cts.com>
2022-05-24 17:06:54 +0530

    commit

---
+++ eureka-art/src/main/java/eurekaserver/eurekaart/EurekaArtApplication.java
@@ -0,0 +1,15 @@
+package eurekaserver.eurekaart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaArtApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(EurekaArtApplication.class, args);
+	}
+
+}
